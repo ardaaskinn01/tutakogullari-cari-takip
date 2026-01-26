fc7f3ed849a7750280198e5f47d8a7c62647db6b
ardaaskinn01 <ardaaskindm@gmail.com>
2026-01-26 16:21:22 +0300

    build updates

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.is_takip"
+    namespace = "com.aasoft.is_takip"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
